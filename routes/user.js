978c96412b50d19583c971f5c3c2b3c238d6c6a9
Manideepsai <nmanideepsai25@gmail.com>
2025-07-23 19:54:25 +0530

    Fix: Added homepage route and view

--- routes/user.js
+++ routes/user.js
@@ -6,11 +6,16 @@ const passport = require("passport");
 const { saveRedirectUrl, isLoggedIn } = require("../middleware.js"); // Make sure the path is correct
 const userController=require("../controllers/user.js")
 
+router.get("/", (req, res) => {
+  res.render("listings/index"); // or "home" if you want to create a separate one
+});
+
 // Signup route
 router.route("/signup")
       .get(userController.renderSignupform)
       .post(
             wrapAsync(userController.signup));
+
 
 
 // Login route
